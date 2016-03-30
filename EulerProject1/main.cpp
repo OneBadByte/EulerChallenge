bd66feb9875fbcf6b79b5ac0b99623666f3ec373
Brody <brody.prestwich@yahoo.com>
2016-03-30 09:41:01 -0600

    added first project

---
+++ EulerProject1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int result = 0;
+    for(int i = 1; i < 1000; i++){
+        if(((i % 3) == 0) || ((i % 5) == 0)){
+
+            result += i;
+            //cout << result;
+
+        }
+
+
+    }
+    cout << result;
+
+
+
+    return 0;
+}
